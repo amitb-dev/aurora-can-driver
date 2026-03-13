eda66db2770b4d82f7e73aed4801713fd96fd30c
Amit <bamit127@gmail.com>
2026-03-13 16:32:58 +0200

    Final fix: Include mock vehicle in data_files

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        # Include all launch files from the 'launch' directory
+        # הוספנו כאן את קובץ המוק כדי שייכלל בהתקנה
+        ('share/' + package_name, ['package.xml', 'mock_aurora_vehicle_node.py']), 
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
